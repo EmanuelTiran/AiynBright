e65375d27d82e06c674a2425a9f14e50ec2d4d3b
Emanuel <eatiran39@gmail.com>
2024-09-19 22:37:47 +0300

    field is working complet

--- components/Field/index.jsx
+++ components/Field/index.jsx
@@ -5,23 +5,43 @@ import Popup from '../Popup';
 
 export default function Field({ user, distanceUser }) {
   const words = ["9", "5", "7", "1", "8", "2", "4", "6", "0", "1", "A", "S", "d", "F", "E", "X", "V", "G", "I", "M", "N", "B", "Z", "W"];
-  const sizes =  [-18, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+  // const sizes =  [-18, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
   ; // ערכים בס"מ למיקום
+  const fillSizes = () => {
+    const sizes = [];
+    for (let i = -24; i <= 24; i += 2) {
+      sizes.push(i);
+    }
+    return sizes;
+  }
+
+  
+
   const [open, setOpen] = useState(false);
   const [isLeft, setIsLeft] = useState(distanceUser?.side === 'left' ? true : false);
+  const [sizes, setSizes] = useState(fillSizes());
   const [distance, setDistance] = useState(distanceUser ? distanceUser.distance : 0);
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
   const [firstTime, setFirstTime] = useState(true);
-  const [currentSizeIndex, setCurrentSizeIndex] = useState(distanceUser ? 1 : 18); // TODO : לעדכן את המרחק מהפאראם לפי האינדקס!!!
+  const [currentSizeIndex, setCurrentSizeIndex] = useState(distanceUser ? sizes.indexOf(Number(distanceUser.distance)) : Math.floor(sizes.length / 2)); // TODO : לעדכן את המרחק מהפאראם לפי האינדקס!!!
+
 
   useEffect(() => {
     if (firstTime) {
       setFirstTime(false);
       setDistance(distanceUser ? distanceUser.distance : sizes[currentSizeIndex]);
-    }else {
+    } else {
       setDistance(sizes[currentSizeIndex]);
     }
   }, [currentSizeIndex]);
+
+  useEffect(() => {
+  console.log({currentSizeIndex})
+  console.log({distanceUser})
+  console.log({sizes})
+  console.log(sizes.indexOf(Number(distanceUser.distance)))
+  console.log(distanceUser.distance)
+  }, []);
 
   const moveRight = () => {
     if (currentSizeIndex < sizes.length - 1) setCurrentSizeIndex(currentSizeIndex + 1);
@@ -64,12 +84,12 @@ export default function Field({ user, distanceUser }) {
   };
 
   return (
-    <div 
-    className={`flex items-center justify-center flex-col w-full mx-auto h-[84vh] bg-[aliceblue] rounded-[2%] border-2 border-[#facc15] border-b border-purple-400 p-4`}
-    style={{
-      borderLeft: isLeft ? '8px solid #facc15' : 'none',
-      borderRight: !isLeft ? '8px solid #facc15' : 'none'
-    }}
+    <div
+      className={`flex items-center justify-center flex-col w-full mx-auto h-[84vh] bg-[aliceblue] rounded-[2%] border-2 border-[#facc15] border-b border-purple-400 p-4`}
+      style={{
+        borderLeft: isLeft ? '8px solid #facc15' : 'none',
+        borderRight: !isLeft ? '8px solid #facc15' : 'none'
+      }}
     >
       <div className="w-full " style={{ transform: `translateX(${distance}cm)`, transition: 'transform 0.3s ease-in-out' }}>
         <p className="text-7xl flex items-center justify-center flex-row w-[96vw] h-[42vh]">
