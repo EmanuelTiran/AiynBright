80745e7beb50f0ce44ebf5683dfd26a48e0fdd20
Emanuel <eatiran39@gmail.com>
2025-01-02 21:49:09 +0200

    update Arr users

--- components/ArrUsers.jsx
+++ components/ArrUsers.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Weaknesses from './Weaknesses';
+import UsersTable from './UsersTable';
 
 function ArrUsers({ users }) {
     const [sortedUsers, setSortedUsers] = useState(users);
@@ -91,56 +92,13 @@ function ArrUsers({ users }) {
                 onChange={handleSearch}
                 className="mb-4 p-2 border border-gray-300 rounded w-full"
             />
-            <table className="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
-                <thead className="bg-gray-200">
-                    <tr>
-                        <th onClick={() => requestSort('username')} className="cursor-pointer p-4 text-left text-center">Username</th>
-                        <th onClick={() => requestSort('email')} className="cursor-pointer p-4 text-left text-center">Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {usersToDisplay.map((user, index) => (
-                        <React.Fragment key={index}>
-                            <tr className="border-t even:bg-gray-50 hover:bg-gray-100 text-center">
-                                <td className="p-4">{user.username}</td>
-                                <td className="p-4">
-                                    <Link href={`mailto:${user.email}`}>{user.email}</Link>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colSpan="2" className="p-4">
-                                    <div className="flex gap-4 justify-center">
-                                        <Weaknesses
-                                            type="color"
-                                            weaknesses={user.colorWeaknesses}
-                                            email={user.email}
-                                            deleteWeakness={deleteWeakness}
-                                            expanded={expandedSection.userId === user.email && expandedSection.type === 'color'}
-                                            onChange={(isExpanded) => handleExpand(user.email, 'color', isExpanded)}
-                                        />
-                                        <Weaknesses
-                                            type="size"
-                                            weaknesses={user.sizeWeaknesses}
-                                            email={user.email}
-                                            deleteWeakness={deleteWeakness}
-                                            expanded={expandedSection.userId === user.email && expandedSection.type === 'size'}
-                                            onChange={(isExpanded) => handleExpand(user.email, 'size', isExpanded)}
-                                        />
-                                        <Weaknesses
-                                            type="field"
-                                            weaknesses={user.fieldWeaknesses}
-                                            email={user.email}
-                                            deleteWeakness={deleteWeakness}
-                                            expanded={expandedSection.userId === user.email && expandedSection.type === 'field'}
-                                            onChange={(isExpanded) => handleExpand(user.email, 'field', isExpanded)}
-                                        />
-                                    </div>
-                                </td>
-                            </tr>
-                        </React.Fragment>
-                    ))}
-                </tbody>
-            </table>
+           <UsersTable
+                usersToDisplay={usersToDisplay}
+                requestSort={requestSort}
+                expandedSection={expandedSection}
+                handleExpand={handleExpand}
+                deleteWeakness={deleteWeakness}
+            />
             {visibleUsersCount < filteredUsers.length && (
                 <button
                     onClick={handleShowMore}
